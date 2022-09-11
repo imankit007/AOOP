a758f92a87cd3666f9740e64e1667e1e7051593c
Ankit <50232341+imankit007@users.noreply.github.com>
2022-09-12 02:40:45 +0530

    updated q1 and a2
    added comments

--- a1q1.java
+++ a1q1.java
@@ -1,5 +1,6 @@
 public class a1q1 {
     public static void main(String[] args) {
+        // Divide by zero exception
         try {
             int a = 30, b = 0;
             int c = a / b;
@@ -7,7 +8,9 @@ public class a1q1 {
         } catch (ArithmeticException e) {
             System.out.println("Can't divide a number by 0");
         }
-
+        // Index out of bound exception
+        // We are referencing to an index which is greater than the last index of the
+        // array
         try {
             int a[] = new int[5];
             a[9] = 133;
@@ -15,7 +18,8 @@ public class a1q1 {
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array Index is Out Of Bounds");
         }
-
+        // Nullpointerexception
+        // The variable has null value but we are trying to refer it
         try {
             String a = null;
             System.out.println(a.charAt(0));
--- a1q2.java
+++ a1q2.java
@@ -7,7 +7,9 @@ public class a1q2 {
         System.out.println("Enter the number: ");
         int input = sc.nextInt();
         sc.close();
-
+        // first check if entered number is negative
+        // if the number is negative throw NegativeNumberNotAllowedException
+        // and if the number is not prime throw NumberNotPrimeException
         try {
             if (input < 0) {
                 throw new NegativeNumberNotAllowedException();
@@ -28,6 +30,9 @@ public class a1q2 {
 
     }
 
+    // this method checks whether the number is prime or not
+    // Input Parameter - num(int)
+    // Return Type - boolean(true if prime, false otherwise)
     public static boolean isPrime(int num) {
 
         for (int i = 2; i < num / 2; i++) {
@@ -39,12 +44,14 @@ public class a1q2 {
     }
 }
 
+// user-defined exception
 class NegativeNumberNotAllowedException extends Exception {
     NegativeNumberNotAllowedException() {
         System.out.print("Negative numbers are not allowed as input!!");
     }
 }
 
+// user-defined exception
 class NumberNotPrimeException extends Exception {
     NumberNotPrimeException() {
         System.out.println("The number entered is not prime!!");
